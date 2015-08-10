55a8dd5c387d1e8144e338c91ba246e833d920be
Victor Kifer <kifervictor@gmail.com>
2015-08-10 22:08:48 +0300

    Moved from python to ipython. Config file created. Started working on random forest

--- input_data.py
+++ input_data.py
@@ -1,23 +1,11 @@
-import os
+from config import *
 
-INPUT_IMAGES = 'input/pin_images.csv'
-INPUT_PINS = 'input/pins.csv'
-INPUT_REPINS = 'input/repins.csv'
-INPUT_USERS = 'input/users.csv'
+INPUT_IMAGES = INPUT_DATA_FOLDER + '/pin_images.csv'
+INPUT_PINS = INPUT_DATA_FOLDER + '/pins.csv'
+INPUT_REPINS = INPUT_DATA_FOLDER + '/repins.csv'
+INPUT_USERS = INPUT_DATA_FOLDER + '/users.csv'
 
-def files_exist(list):
-    '''
-    Checks whether the input data exists by given path
-    '''
-    input_data_exists = True
-    for file in list:
-        if not os.path.exists(file):
-            print 'Input data not found:', file
-            input_data_exists = False
+INPUT_TRAIN_FILE = INPUT_DATA_FOLDER + '/repins.train.csv'
+INPUT_TEST_FILE = INPUT_DATA_FOLDER + '/repins.test.csv'
 
-    return input_data_exists
-
-def check_input_data_exists():
-    input_files = [INPUT_IMAGES, INPUT_PINS, INPUT_REPINS, INPUT_USERS]
-
-    return files_exist(input_files)
+INPUT_NEGATIVE_REPINS_FILE = INPUT_DATA_FOLDER + '/repins.neg.csv'
