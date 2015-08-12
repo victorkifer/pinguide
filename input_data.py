266f35e54e54c9a219532a9049b725dbfce8f6a9
Victor Kifer <kifervictor@gmail.com>
2015-08-12 12:16:43 +0300

    Create data samples, train and test sets, extend random forest for all train set

--- input_data.py
+++ input_data.py
@@ -1,11 +1,25 @@
 from config import *
 
 INPUT_IMAGES = INPUT_DATA_FOLDER + '/pin_images.csv'
-INPUT_PINS = INPUT_DATA_FOLDER + '/pins.csv'
-INPUT_REPINS = INPUT_DATA_FOLDER + '/repins.csv'
-INPUT_USERS = INPUT_DATA_FOLDER + '/users.csv'
+INPUT_PINS = INPUT_DATA_FOLDER + '/pins.sample.csv'
+INPUT_REPINS = INPUT_DATA_FOLDER + '/repins.sample.csv'
+INPUT_USERS = INPUT_DATA_FOLDER + '/users.sample.csv'
+
+INPUT_IMAGES_ORIGIN = INPUT_DATA_FOLDER + '/pin_images.csv'
+INPUT_PINS_ORIGIN = INPUT_DATA_FOLDER + '/pins.csv'
+INPUT_REPINS_ORIGIN = INPUT_DATA_FOLDER + '/repins.csv'
+INPUT_USERS_ORIGIN = INPUT_DATA_FOLDER + '/users.csv'
+
+INPUT_IMAGES_SAMPLE = INPUT_DATA_FOLDER + '/pin_images.sample.csv'
+INPUT_PINS_SAMPLE = INPUT_DATA_FOLDER + '/pins.sample.csv'
+INPUT_REPINS_SAMPLE = INPUT_DATA_FOLDER + '/repins.sample.csv'
+INPUT_USERS_SAMPLE = INPUT_DATA_FOLDER + '/users.sample.csv'
 
 INPUT_TRAIN_FILE = INPUT_DATA_FOLDER + '/repins.train.csv'
 INPUT_TEST_FILE = INPUT_DATA_FOLDER + '/repins.test.csv'
 
-INPUT_NEGATIVE_REPINS_FILE = INPUT_DATA_FOLDER + '/repins.neg.csv'
+INPUT_NEGATIVE_REPINS = INPUT_DATA_FOLDER + '/repins.neg.csv'
+INPUT_NEGATIVE_REPINS_TRAIN = INPUT_DATA_FOLDER + '/repins.train.neg.csv'
+INPUT_NEGATIVE_REPINS_TEST = INPUT_DATA_FOLDER + '/repins.test.neg.csv'
+
+INPUT_CATEGORIES = INPUT_DATA_FOLDER + '/formats/category_id.txt'
