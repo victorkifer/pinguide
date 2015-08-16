9b0b9cb352640f43a4582b82ec8e63fefa29b7d7
Victor Kifer <kifervictor@gmail.com>
2015-08-16 09:06:13 +0300

    Images downloading added

--- server/pinguide_api/views.py
+++ server/pinguide_api/views.py
@@ -1,8 +1,11 @@
 import json
+import sys
 from django.http import HttpResponse
 
 from models import *
 
+sys.path.append('..')
+from pinguide_logic import Downloader
 
 def index(req):
   return HttpResponse("PinGuide API")
@@ -10,15 +13,18 @@ def index(req):
 
 def recommend(req):
   nickname = req.GET.get('nickname', None)
-  board_id = req.GET.get('board_id', None)
+  board_name = req.GET.get('board_name', None)
 
   response = None
-  if nickname is None or board_id is None:
+  if nickname is None or board_name is None:
     response = {
       'status': -1,
       'error': 'Required fields: nickname, board_id'
     }
   else:
+    Downloader.fetch_images(nickname, board_name)
+    dir = Downloader.get_dir(nickname, board_name)
+
     images = Image.objects.all()[:20]
     json_data = [image.as_json() for image in images]
 
---
+++ server/pinguide_logic/Downloader.py
@@ -0,0 +1,65 @@
+__author__ = 'victor'
+
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+import time
+import os
+import shutil
+
+##########################################################
+def __fetch_file(link) :
+  # user_agent  = 'curl/7.29.0'
+  # host = 'www.pinterest.com'
+  # accept = '*/*'
+
+  url = link
+  # user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36'
+  # values = {}
+  # headers = {"User-Agent" : user_agent, "Host" : host, "Accept" : accept} #{'User-Agent' : user_agent }
+  # headers = {} #{'User-Agent' : user_agent }
+  # data = urllib.urlencode(values)
+  req = urllib2.Request(url) #, data, headers)
+  response = urllib2.urlopen(req)
+  the_page = response.read()
+  return the_page
+
+#########################################################
+def get_link(nickname, board_name):
+  return 'https://www.pinterest.com/' + nickname + '/' + board_name + '/'
+
+#########################################################
+def get_dir(nickname, board_name):
+  return 'out/' + nickname + '/' + board_name + '/'
+
+##########################################################
+def ensure_dir(dir):
+  if not os.path.exists(dir):
+    os.makedirs(dir)
+
+##########################################################
+def fetch_images(nickname, board_name):
+  url = get_link(nickname, board_name)
+  dir = get_dir(nickname, board_name)
+  ensure_dir(dir)
+
+  html_code = __fetch_file(url)
+  soup = BeautifulSoup(html_code, 'html.parser')
+
+  for link in soup.find_all('img'):
+    if not link.has_attr("class"):
+      continue
+    if not "pinImg" in link["class"]:
+      continue
+    time.sleep(0.1)
+    link = link["src"]
+    filename = link[link.rindex("/"):]
+    with open(dir + filename, 'wb+') as f:
+      f.write(__fetch_file(link))
+
+##########################################################
+def remove_images(nickname, board_name):
+  dir = get_dir(nickname, board_name)
+  shutil.rmtree(dir, True)
+
+# fetch_images('https://www.pinterest.com/nick_goodey/under-the-sea/')
