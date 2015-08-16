ec37640b6dc5572fcf893b650a972ebc43de16b0
Victor Kifer <kifervictor@gmail.com>
2015-08-17 00:02:16 +0300

    web interface implemented

---
+++ server/pinguide_web/static/js/pinguide.login.js
@@ -0,0 +1,51 @@
+function loadRecommendationFor(nickname, board_name) {
+    if (nickname == undefined || board_name == undefined
+            || nickname == '' || board_name == ''){
+        alert("Please, complete all required fields!")
+        return;
+      }
+
+      $("#modalWindow").modal('show')
+
+      data = {
+        nickname: nickname,
+        board_name: board_name
+      }
+
+      $.get("/api/v1/recommend", data)
+        .done(function( response ) {
+            $("#modalWindow").modal('hide')
+
+            if (response['status'] != 0) {
+              alert(response['error']);
+              return;
+            }
+
+            var images = response['data'];
+            var image_urls = []
+            for (var i = 0; i < images.length; i++) {
+                image_urls.push(images[i]['url']);
+            }
+            displayImages(image_urls)
+        })
+        .fail(function(jqXHR, textStatus) {
+            $("#modalWindow").modal('hide')
+            alert('An error occurred: ' + textStatus);
+        });
+}
+
+function displayImages(images) {
+    $('#header').html('<p align="center" class="header">Your recommendations</p>')
+
+	html = '';
+	for (var i = 0; i < images.length; i++) {
+	    html += '<div class="imgholder"><img src="' + images[i] + '" /></div>';
+	}
+	$('#content').html(html);
+
+	$('#content').BlocksIt({
+	    numOfCol: 4,
+	    offsetX: 8,
+	    offsetY: 8
+	});
+}
